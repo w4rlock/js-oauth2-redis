f96558cc6a305efb451c5c52a991adb1ec188f8e
emanuel <emanuel@rockboxstudios.com>
2016-04-25 11:22:26 -0300

    register client

--- public/src/services/http.js
+++ public/src/services/http.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
  * Responsible for all HTTP requests.
  */
 export default {
+<<<<<<< 45a0c70d634ca5b9528cb8c04e0b95f80e753e3e
     request(method, url, data, doneCb = null, errorCb = null) {
         return Vue.http[method](url, data).then(doneCb, errorCb);
     },
@@ -44,6 +45,28 @@ export default {
 			return str;
 		},
 
+=======
+    request(method, url, data, successCb = null, errorCb = null) {
+        return Vue.http[method](url, data).then(successCb, errorCb);
+    },
+
+    get(url, successCb = null, errorCb = null) {
+        return this.request('get', url, {}, successCb, errorCb);
+    },
+
+    post(url, data, successCb = null, errorCb = null) {
+        return this.request('post', url, data, successCb, errorCb);
+    },
+
+    put(url, data, successCb = null, errorCb = null) {
+        return this.request('put', url, data, successCb, errorCb);
+    },
+
+    delete(url, data = {}, successCb = null, errorCb = null) {
+        return this.request('delete', url, data, successCb, errorCb);
+    },
+
+>>>>>>> register client
     /**
      * A shortcut method to ping and check if the user session is still valid.
      */
